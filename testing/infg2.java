5263db33b2bde08716871d6f68c48cc552b0985f
3 <41834061+No3Mc@users.noreply.github.com>
2023-02-15 18:58:43 +0000

    testing/infg: Trying to save up

---
+++ testing/infg2.java
@@ -0,0 +1,50 @@
+package testing;
+Class Example
+
+{
+    public static void main (Strings args[])
+    {
+    example (args);
+    for (int j = 0; j < 5; j++)
+    {
+        j--;
+        for (int i=0; i<n; ++i)
+        {
+            
+            System.out.print(args[i + " " ]);
+        }
+        
+    }
+ } 
+    public static String example(String args[])
+    {
+        int t = 10
+        int r =100;
+
+        int r = t * r;
+        return r;
+    }
+}
+
+class Example {
+
+    public static void main(String[] args) 
+    {
+        int n = example(args);
+        for (int j = 0; j < 5; j++) {
+            j--;
+            for (int i = 0; i < n; i++) {
+                System.out.print(args[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int example(String[] args) 
+    {
+        int t = 10;
+        int r = 100;
+        r = t * r;
+        return r;
+    }
+}
