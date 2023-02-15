9fa2a9356010556dd88650dc3f334e6b0ee9d4a7
3 <41834061+No3Mc@users.noreply.github.com>
2023-02-15 05:13:16 +0000

    index: Update. And this time hope so that node js works too

--- index.js
+++ index.js
@@ -1,87 +1,42 @@
-// Import the modules
-const request = require('request');
-const csvWriter = require('csv-writer');
-
-// Get the environment variables
-const api_key = process.env['8d33bc0f-f0b1'];
-const api_token = process.env['WrZxf40Y3lEvBB2t3J6bVs'];
-
-const headers = {
-  'X-Auth-Key': "8d33bc0f-f0b1",
-  'X-Auth-Token': "WrZxf40Y3lEvBB2t3J6bVs"
-};
-
-const base_url = 'https://app.arrivy.com/api';
-
-const customer_data = [];
-
-const getCustomerDetails = (customer_id) => {
-  return new Promise((resolve, reject) => {
-    request.get(base_url + `/customers/${customer_id}`, {headers: headers}, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else if (response.statusCode === 200) {
-        const customer_details = JSON.parse(body);
-        const name = customer_details.name;
-        const email = customer_details.email;
-        const phone = customer_details.phone;
-        const address = customer_details.address;
-        const customer_dict = {
-          name: name,
-          email: email,
-          phone: phone,
-          address: address
-        };
-        resolve(customer_dict);
-      } else {
-        reject(`Error getting customer details for ${customer_id}: ${response.statusCode}`);
-      }
-    });
+var request = require('request');
+var fs = require('fs');
+var csvWriter = require('csv-writer');
+var apiKey = '8d33bc0f-f0b1';
+var authToken = 'WrZxf40Y3lEvBB2t3J6bVs';
+var apiUrl = 'https://app.arrivy.com/api/customers';
+var csvFile = 'customers.csv';
+request.get({
+  url: apiUrl,
+  headers: {
+    'X-ARRIVY-API-KEY': apiKey,
+    'X-ARRIVY-AUTH-TOKEN': authToken // Add the auth token header
+  }
+}, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error('Status code: ' + response.statusCode);
+    return;
+  }
+  var data = JSON.parse(body);
+  var customers = data.map(function (customer) {
+    return {
+      name: customer.name,
+      email: customer.email,
+      phone: customer.phone
+    };
   });
-};
-
-const writeCustomerData = (customer_data) => {
-  return new Promise((resolve, reject) => {
-    const writer = csvWriter.createObjectCsvWriter({
-      path: 'customer_data.csv',
-      header: [
-        {id: 'name', title: 'name'},
-        {id: 'email', title: 'email'},
-        {id: 'phone', title: 'phone'},
-        {id: 'address', title: 'address'}
-      ]
-    });
-    writer.writeRecords(customer_data)
-      .then(() => {
-        resolve('The CSV file was written successfully');
-      })
-      .catch((error) => {
-        reject(error);
-      });
+  var csvWriter = csvWriter.createArrayCsvWriter({
+    path: csvFile,
+    header: ['Name', 'Email', 'Phone']
   });
-};
-request.get(base_url + '/customers', {headers: headers}, (error, response, body) => {
-    if (error) {
+  csvWriter.writeRecords(customers)
+    .then(function () {
+      console.log('Customer data written to ' + csvFile);
+    })
+    .catch(function (error) {
       console.error(error);
-  } else if (response.statusCode === 200) {
-      const customers = JSON.parse(body);
-      const promises = [];
-      for (const customer of customers) {
-        const customer_id = customer.id;
-        promises.push(getCustomerDetails(customer_id));
-      }
-      Promise.all(promises)
-        .then((results) => {
-          for (const result of results) {
-            customer_data.push(result);
-          }
-          return writeCustomerData(customer_data);
-        })
-        .then((message) => {
-          console.log(message);
-        })
-        .catch((error) => {
-            console.error(error);
-          });
-    }
+    });
 });
