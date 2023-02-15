f50a5989d9065b6ebd02a84daf0989e00d284dd5
Qaisar7566 <qaisar.abbas@arrivy.com>
2023-02-16 02:10:45 +0500

    index: Update

--- index.js
+++ index.js
@@ -1,46 +1,18 @@
-<<<<<<< HEAD
-const axios = require('axios');
-
-const authToken = 'WrZxf40Y3lEvBB2t3J6bVs';
-const headers = {
-  'Authorization': `Bearer ${authToken}`,
-  'Content-Type': 'application/json'
-};
-const url = 'https://app.arrivy.com/api/';
-
-axios.get(url, { headers })
-  .then(response => {
-    if (response.status === 200) {
-      const customerData = response.data;
-      // fs.writeFile('customers.json', JSON.stringify(customerData), err => {
-      //   if (err) {
-      //     console.error(err);
-      //   } else {
-      //     console.log('Customer data saved to customers.json');
-      //   }
-      // });
-    } else {
-      console.error(`Error retrieving customer data: ${response.status}`);
-    }
-  })
-  .catch(error => {
-=======
 var request = require('request');
 var fs = require('fs');
 var csvWriter = require('csv-writer');
 var apiKey = '8d33bc0f-f0b1';
 var authToken = 'WrZxf40Y3lEvBB2t3J6bVs';
-var apiUrl = 'https://app.arrivy.com/api/customers';
+var apiUrl = 'https://app.arrivy.com/api/';
 var csvFile = 'customers.csv';
 request.get({
   url: apiUrl,
   headers: {
-    'X-ARRIVY-API-KEY': apiKey,
-    'X-ARRIVY-AUTH-TOKEN': authToken // Add the auth token header
+    'X-API-KEY': apiKey,
+    'X-AUTH-TOKEN': authToken
   }
 }, function (error, response, body) {
   if (error) {
->>>>>>> e5b705e4aa42f629ba028ac8b4575bdeeca77526
     console.error(error);
     return;
   }
