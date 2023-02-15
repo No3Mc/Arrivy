5b4574e62f92f036bbf04b2d54b2c1a9628947bd
3 <41834061+No3Mc@users.noreply.github.com>
2023-02-15 00:49:13 +0000

    run: Testing the update

--- run.py
+++ run.py
@@ -1,44 +1,40 @@
 import requests
-import csv
-
-# the URL
-
-base_url = "https://app.company.com/api/users/profile"
-
-#entity_ids
-entity_ids = [1, 2, 3] #testing it idk if it should work or not
-
-#params
-
-params = {
-    "name" =: True
-}
+from flask import Flask, request, jsonify
+import json # import the json module
 
 
-# list for names
-
-names = []
-
-# loop through the entity ids
-
-    for entity_id in entity_ids:
-        # get request to the api with params and headers
-
-        response = requests.get(base_url + str(entity_id), params=params, headers = headers)
-        # parse response content as json
-        data = response.json()
-        # get the name from the data
+app = Flask (__name__)
 
 
-# auth headers
+# endpoint for the customers api
+@app.route('/customers', methods=['GET'])
+def get_customers():
+    url = 'https://api.arrivy.app/api/v2/customers/'
+    headers = {
+        'X-Auth-Key': '8d33bc0f-f0b1',
+        'X-Auth-Token': 'WrZxf40Y3lEvBB2t3J6bVs'
+    }
+    response = requests.get(url, headers=headers)
+    customers = response.json()
 
-headers = {
-    "X-Auth-Key" : "",
-    "X-Auth-Token" : ""
-}
+    # write the customers list to a JSON file
+    with open('customers.json', 'w') as file: # open a file in write mode
+        json.dump(customers, file) # write the customers list to the file
 
-# get request
+    # get from the url
+    page = int(request.args.get('page', 1));
+    items_per_page = int(request.args.get('items_per_page', 500));
+    external_id = request.args.get('external_id', None); #testing ph
+    group_id = request.args.get('group_id', None) #testing ph
 
-response = response.get(url, headers = headers)
-
-#
+    #filter custs
+    if external_id:
+        customers = [c for c in customers if str(c.get('external_id')) == str(external_id)]
+    if group_id:
+        customers = [c for c in customers if str (c.get('group_id')) == str(group_id)]
+    start_index = (page - 1) * items_per_page
+    end_index = start_index + items_per_page
+    customers = customers[start_index:end_index]
+    
+    # return the filtered customers as a JSON response
+    return jsonify(customers)
